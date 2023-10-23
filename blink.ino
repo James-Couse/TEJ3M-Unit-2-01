145fb777557fb59f0916ba22ad6d3ecaf516c17c
James Couse <144252195+James-Couse@users.noreply.github.com>
2023-10-23 15:16:21 +0000

    added .py blink

--- blink.ino
+++ blink.ino
@@ -1,12 +1,13 @@
-// C++ code
-//
-void setup()
-{
+/*
+ * Created by: James couse
+ * created on: Oct 23 2023
+ * Blinks the builtin LED on an Arduino Uno.
+ */
+void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(LED_BUILTIN, LOW);
--- code.py
+++ code.py
@@ -4,12 +4,13 @@ Created on: Oct 23 2023
 This module turns the builtin LED on and off.
 """
 
+import time
 import board
 import digitalio
-
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
-    led.value = True
+    led.value = not led.value
+    time.sleep(1)
